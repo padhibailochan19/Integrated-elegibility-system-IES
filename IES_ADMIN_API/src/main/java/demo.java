18982d06fcf1bc0d2933905f189b2e6c095451e1
padhibailochan19 <padhibailochan19@gmail.com>
2024-08-11 09:25:39 +0530

    file changes

---
+++ IES_ADMIN_API/src/main/java/demo.java
@@ -0,0 +1,8 @@
+
+public class demo {
+//		int x;
+	public static void main(String[] args) {
+		int x;
+		System.out.println();
+	}
+}
---
+++ IES_AR_API/src/test/java/in/bailochan/ArApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bailochan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
